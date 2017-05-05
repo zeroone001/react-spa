51a0ba5b486d758c5c9cb12a1d459c2de1547b0a
liuyongsheng <liuyongsheng@smzdm.com>
2017-05-05 14:17:15 +0800

    完善配置

--- src/components/author/List.js
+++ src/components/author/List.js
@@ -16,4 +16,36 @@ class List extends Component {
             }
         }
     }
+
+    componentWillMount() {
+        fetch.get('/authorlist.json').then((res) => {
+            // 实际项目中，这里最好判断一下组件是否已经unmounted
+            this.setState({
+                data: res.data
+            })
+        })
+    }
+    render() {
+        // 这里可以根据data的状态，返回其它内容，例如
+        // if (!this.state.data) return <Loading />
+
+        return (
+            <Card>
+                <Card.Header>作者列表</Card.Header>
+                <Table
+                  data={this.state.data.list}
+                  columns={[
+                    { name: 'id', header: 'ID' },
+                    { name: 'name', header: '姓名' },
+                    { name: 'nationality', header: '国籍' },
+                    { name: 'birthday', header: '生日' },
+                  ]}
+                />
+            </Card>
+        )
+    }
+
+
 }
+
+export default List
--- src/components/author/index.js
+++ src/components/author/index.js
@@ -1,5 +1,33 @@
-export default function() {
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+    Route,
+    Switch
+} from 'react-router-dom'
+import List from './List'
+import Edit from './Edit'
+
+function Author(props) {
+    const {
+        url
+    } = props.match
+
     return (
-        <div>书籍列表</div>
+        <Switch>
+      <Route path={`${url}/new`} component={Edit} />
+      <Route
+        path={`${url}/edit/:id`}
+        render={
+          ({ history, match }) => <Edit history={history} fetch={{ url: `/api/author/${match.params.id}` }} />
+        }
+      />
+      <Route path={`${url}`} component={List} />
+    </Switch>
     )
 }
+
+Author.propTypes = {
+    match: PropTypes.object.isRequired,
+}
+
+export default Author
